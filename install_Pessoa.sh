558bd2307a36c5f008601582694e61e7a7c77cd6
tasoglou <tasoglou@5cb82b16-ab79-11dd-9e34-9d61e98118c0>
2013-06-12 20:39:07 +0000

    New build system

--- install_Pessoa.sh
+++
@@ -1,7 +0,0 @@
-cp ./Pessoa_abstract/bin/* ./Pessoa_1.4/;
-cp ./Pessoa_control/bin/* ./Pessoa_1.4/;
-cp ./Pessoa_design/bin/* ./Pessoa_1.4/;
-cp ./utils/bin/* ./Pessoa_1.4/;
-cp ./library/Matlab/* ./Pessoa_1.4/;
-
-
